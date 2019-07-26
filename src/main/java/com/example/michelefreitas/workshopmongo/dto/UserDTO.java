78ad3fed1c6bcd3686f8d7de917cbbe0259459bd
Michele Freitas <michele-braga@hotmail.com>
2019-07-26 11:54:47 +0100

    Usando padrao DTO para retornar usuarios

---
+++ src/main/java/com/example/michelefreitas/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,51 @@
+package com.example.michelefreitas.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.example.michelefreitas.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+		
+	}
+	
+	//forma automatizada instancios o UserDTO a partir do User. Instancia UserDTO a partir do User
+	public UserDTO(User obj) {
+		//copia os dados do User para o DTO
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
--- src/main/java/com/example/michelefreitas/workshopmongo/resource/UserResource.java
+++ src/main/java/com/example/michelefreitas/workshopmongo/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.michelefreitas.workshopmongo.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.michelefreitas.workshopmongo.domain.User;
+import com.example.michelefreitas.workshopmongo.dto.UserDTO;
 import com.example.michelefreitas.workshopmongo.service.UserService;
 
 @RestController
@@ -22,8 +24,9 @@ public class UserResource {
 	//método que vai usar é get que serve para obter informações no padrão rest
 	//outra forma é o @GetMapping
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){//método retorna lista usuário. Busca todos. Acresc. o Response... ele retorna obj sofisticado(encapsula estrutura necessária retornar resposta http com possíveis cabeçalhos, erros... )
-		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);//ok vai instanciar Response... já com o cód http com sucesso. body é corpo da resposta
+	public ResponseEntity<List<UserDTO>> findAll(){//método retorna lista usuário. Busca todos. Acresc. o Response... ele retorna obj sofisticado(encapsula estrutura necessária retornar resposta http com possíveis cabeçalhos, erros... )
+		List<User> list = service.findAll();//carrega lista usuário
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); // converte cada um para DTO. stream transformar em coleção compatível. .map pega cada objeto x da lista original e para cada objeto x da lista orig..., para cada objeto desse q será um usuário
+		return ResponseEntity.ok().body(listDto);//ok vai instanciar Response... já com o cód http com sucesso. body é corpo da resposta
 	}
 }
